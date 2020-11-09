c49495d3f7d6150989ec7d5250d6759dd8b8a5d3
zeljko999 <zeljkoudo999@gmail.com>
2020-11-09 16:49:07 +0100

    Komit nakon kreiranja modela klase

---
+++ Excersise_45-2018/Excersise_45-2018/ExerciseResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excersise_45_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndex, int points)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            Points = points;
+        }
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+        public override string ToString()
+        {
+            return "Id: " + Id + "Student: " + StudentName + "Index: " +StudentIndex + "Points: " + Points;
+        }
+
+    }
+}
