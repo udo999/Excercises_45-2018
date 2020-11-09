9173715e76fa0918f5cba210c75165a794fca2e0
zeljko999 <zeljkoudo999@gmail.com>
2020-11-09 16:23:31 +0100

    Drugi kompit posle pravljenja vaze FacultyDB i tabele ExerciseResults

---
+++ Excersise_45-2018/Excersise_45-2018/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ExerciseResult]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StudentName] NVARCHAR(100) NOT NULL, 
+    [StudentIndex] NVARCHAR(20) NOT NULL, 
+    [Points] INT NOT NULL
+)
